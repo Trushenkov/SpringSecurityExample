24263bfe23b9906247a934ed05a970833ae5c3d3
Трушенков Дмитрий <diman28051999@gmail.com>
2018-12-11 15:53:13 +0300

    Created SecurityApplication - launch class

---
+++ src/main/java/com/example/security/SecurityApplication.java
@@ -0,0 +1,17 @@
+package com.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
+public class SecurityApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
