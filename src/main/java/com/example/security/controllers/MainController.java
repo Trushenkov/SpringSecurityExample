7c2c789cb79d530f8c099dd6525bad417ed400b1
Трушенков Дмитрий <diman28051999@gmail.com>
2018-12-25 14:17:55 +0300

    edited controller for url "/statistic"

--- src/main/java/com/example/security/controllers/MainController.java
+++ src/main/java/com/example/security/controllers/MainController.java
@@ -1,26 +1,32 @@
 package com.example.security.controllers;
 
+import com.example.security.Person;
+import com.example.security.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpSession;
+import java.sql.Date;
 
 @Controller
 public class MainController {
 
-    @GetMapping(value = "/login")
-    public String getLoginPage() {
-        return "login";
+    private PersonRepository repository;
+
+    @Autowired
+    public MainController(PersonRepository repository) {
+        this.repository = repository;
     }
 
-    @GetMapping(value="/home")
-    public String getHomePage() {
-        return "home";
+    @RequestMapping("/statistic")
+    public String getEntriesPage(Model model, @RequestParam(value  = "login")  String login,
+                                 @RequestParam(value  = "dateFrom") @DateTimeFormat(pattern = "yyyy-MM-dd") String dateFrom,
+                                 @RequestParam(value  = "dateTo") @DateTimeFormat(pattern = "yyyy-MM-dd") String dateTo) {
+        Person person = repository.getCountOfEntries(login, Date.valueOf(dateFrom), Date.valueOf(dateTo));
+        model.addAttribute("count", person.getCountEntries());
+        return "statistic";
     }
-
-    @GetMapping(value="/incorrect")
-    public String getIncorrectPage() {
-        return "incorrect";
-    }
-
 }
